3b5d02d8a035a3fc9ba3101e3674b9241b396e35
Ilya Balbasov <93926843+fxxnxx@users.noreply.github.com>
2022-04-25 11:22:29 +0300

    Update PZ_12_2.py

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -12,7 +12,7 @@ def close():
     root.quit()
 
 
-def generate(length):  # Генерация случайных символов
+def generate(length):  # Функция, генерирующая случайные символы
     global rand_string
     letters_and_digits = string.ascii_letters + string.digits
     rand_string = ''.join(random.sample(letters_and_digits, length))
@@ -20,24 +20,24 @@ def generate(length):  # Генерация случайных символов
     lb0 = Label(text='')
     lb0.place(x=400, y=231, width=800, anchor='center')
     lb3 = Label(text=rand_string, font=('Times', 17))
-    lb3.place(x=530, y=180, anchor='center') # Результат генерации
+    lb3.place(x=400, y=230, anchor='center')
 
 
 root = Tk()
 root.title("Генератор")
-root.geometry('900x400') # Окно
+root.geometry('800x300')
 
 
 lb1 = Label(text='Введите количество символов, которое нужно сгенерировать (не больше 62): ', font=('Times', 15))
 lb2 = Label(text='Результат:', font=('Times', 15))
 
 
-lb1.place(x=450, y=10, anchor='center')  # Ввод
+lb1.place(x=400, y=15, anchor='center')  # Ввод
 lb2.place(x=400, y=180, anchor='center')  # Результат
 
 
 ent1 = Entry(bd=2, width=24)
-ent1.place(x=450, y=70, anchor='center') # Поле ввода
+ent1.place(x=400, y=70, anchor='center')
 
 
 def bttn_clicked():
@@ -45,7 +45,7 @@ def bttn_clicked():
     generate(x)
 
 
-bt1 = Button(text='Сгенерировать', width=15, font=('Times', 14), command=bttn_clicked)
-bt1.place(x=450, y=110, anchor='center') # Кнопка сгенерировать
+bt1 = Button(text='Сгенерировать', width=14, font=('Times', 14), command=bttn_clicked)
+bt1.place(x=400, y=110, anchor='center')
 
 root.mainloop()
